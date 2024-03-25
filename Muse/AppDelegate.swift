fc3064554e24fa40cd858494a32bc37a927c9a02
CGM <cgmajoor@gmail.com>
2024-03-25 12:42:22 +0100

    add navigation controller

--- Muse/AppDelegate.swift
+++ Muse/AppDelegate.swift
@@ -14,10 +14,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
-        window?.rootViewController = ViewController()
+
+        window?.rootViewController = createNavigationController()
         window?.makeKeyAndVisible()
         return true
     }
 
 }
 
+private extension AppDelegate {
+    
+    func createNavigationController() -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: ArtworkListViewController())
+
+        // TODO: Refactor
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithOpaqueBackground()
+        navigationBarAppearance.backgroundColor = UIColor.systemGray6
+        navigationBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.label]
+        navigationBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.label]
+        navigationController.navigationBar.standardAppearance = navigationBarAppearance
+        navigationController.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+
+        return navigationController
+    }
+}
